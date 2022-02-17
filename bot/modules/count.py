167754553e60ed5d2ee866bb32b64c3d7a0b3038
Manik54 <97982101+Manik54@users.noreply.github.com>
2022-02-17 21:57:18 +0530

    Update count.py

--- bot/modules/count.py
+++ bot/modules/count.py
@@ -38,7 +38,7 @@ def countNode(update, context):
         gd = GoogleDriveHelper()
         result = gd.count(link)
         deleteMessage(context.bot, msg)
-        cc = f'\n\n<b>cc: </b>{tag}'
+        cc = f'\n\n<b>🧑🏻‍🦰 Credit: </b>{tag}'
         sendMessage(result + cc, context.bot, update)
         if gdtot_link:
             gd.deletefile(link)
