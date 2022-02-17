e4a09312256931c070596ee854b45c3744cba8b6
Manik54 <97982101+Manik54@users.noreply.github.com>
2022-02-17 22:04:52 +0530

    Update mirror_status.py

--- bot/modules/mirror_status.py
+++ bot/modules/mirror_status.py
@@ -17,8 +17,8 @@ def mirror_status(update, context):
             total, used, free, _ = disk_usage(DOWNLOAD_DIR)
             free = get_readable_file_size(free)
             message = 'No Active Downloads !\n___________________________'
-            message += f"\n<b>CPU:</b> {cpu_percent()}% | <b>FREE:</b> {free}" \
-                       f"\n<b>RAM:</b> {virtual_memory().percent}% | <b>UPTIME:</b> {currentTime}"
+            message += f"\n🖥️<b>CPU:</b> {cpu_percent()}% | 🗑️<b>FREE:</b> {free}" \
+                       f"\n🗄️<b>RAM:</b> {virtual_memory().percent}% | ⏳<b>UPTIME:</b> {currentTime}"
             reply_message = sendMessage(message, context.bot, update)
             Thread(target=auto_delete_message, args=(context.bot, update.message, reply_message)).start()
             return
