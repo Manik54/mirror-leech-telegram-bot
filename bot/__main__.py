a214c9a8f2e400e52f4255e2424ca73cfd3b7c75
Manik54 <97982101+Manik54@users.noreply.github.com>
2022-02-17 23:31:06 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -59,8 +59,8 @@ def stats(update, context):
 
 def start(update, context):
     buttons = ButtonMaker()
-    buttons.buildbutton("Repo", "https://www.github.com/anasty17/mirror-leech-telegram-bot")
-    buttons.buildbutton("Report Group", "https://t.me/+PRRzqHd31XY3ZWZk")
+    buttons.buildbutton("💡 Repo 💡", "https://github.com/Manik54/mirror-leech-telegram-bot")
+    buttons.buildbutton("🎭 Report Group 🎭", "https://t.me/+g_YPJYvkBpI0MTll")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
